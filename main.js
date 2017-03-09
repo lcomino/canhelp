da8fb446628c340f118563800fafe2753a509a67
Lucas Comino <lucas@inovaclick.com.br>
2017-03-09 13:34:02 -0300

    alterando porta

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root : __dirname});
 });
 
-app.listen(3000, () => {
-    console.log('Running on port 3000')
+app.listen(port, () => {
+    console.log('Running on port '+ port)
 });
